ee5af332a87ff099286f5043484b85bb05c2687b
Clinton Graham <ctgraham@pitt.edu>
2019-11-19 06:38:41 -0500

    netbeans autoformat

--- tools/cli.php
+++ tools/cli.php
@@ -12,99 +12,98 @@
  *
  * @brief CLI tool to generate fake metrics data
  */
-
 require(dirname(__FILE__) . '/bootstrap.inc.php');
 
 class CLITool extends CommandLineTool {
 
-	/**
-	 * Constructor.
-	 * @param $argv array command-line arguments
-	 */
-	function __construct($argv = array()) {
-		parent::__construct($argv);
+    /**
+     * Constructor.
+     * @param $argv array command-line arguments
+     */
+    function __construct($argv = array()) {
+	parent::__construct($argv);
 
-		if (sizeof($this->argv) < 1) {
-			$this->usage();
-			exit(1);
-		}
-
-		$this->command = array_shift($this->argv);
+	if (sizeof($this->argv) < 1) {
+	    $this->usage();
+	    exit(1);
 	}
 
-	/**
-	 * Print command usage information.
-	 */
-	function usage() {
-		echo "A generic CLI tool test.\n";
-	}
+	$this->command = array_shift($this->argv);
+    }
 
-	/**
-	 * Delete submission data and associated files
-	 */
-	function execute() {
-		$request = Application::get()->getRequest();
+    /**
+     * Print command usage information.
+     */
+    function usage() {
+	echo "A generic CLI tool test.\n";
+    }
 
-		import('lib.pkp.classes.core.APIRouter');
-		$router = new APIRouter();
-		$router->setApplication(Application::get());
-		$request->setRouter($router);
-		$request->_protocol = 'https';
+    /**
+     * Delete submission data and associated files
+     */
+    function execute() {
+	$request = Application::get()->getRequest();
 
-		// Should get handler from command and route to
-		// api/v1/{slug}/index.php
-		import('api.v1.contexts.ContextHandler');
-		$handler = new ContextHandler();
+	import('lib.pkp.classes.core.APIRouter');
+	$router = new APIRouter();
+	$router->setApplication(Application::get());
+	$request->setRouter($router);
+	$request->_protocol = 'https';
 
-		$userId = 1; // should be admin. todo: get the actual admin
+	// Should get handler from command and route to
+	// api/v1/{slug}/index.php
+	import('api.v1.contexts.ContextHandler');
+	$handler = new ContextHandler();
 
-		// Set up the user
-		$user = Services::get('user')->get($userId); // should get actual admin
-		Registry::set('user', $user);
+	$userId = 1; // should be admin. todo: get the actual admin
+	// Set up the user
+	$user = Services::get('user')->get($userId); // should get actual admin
+	Registry::set('user', $user);
 
-		// Seet up the session
-		$session = SessionManager::getManager()->sessionDao->newDataObject();
-		$session->setId(1);
-		$session->setUserId($userId);
-		$session->setIpAddress(123);
-		$session->setUserAgent('');
-		$session->setSecondsCreated(time());
-		$session->setSecondsLastUsed(time());
-		$session->setDomain('');
-		$session->setSessionData('');
-		SessionManager::getManager()->userSession = $session;
+	// Seet up the session
+	$session = SessionManager::getManager()->sessionDao->newDataObject();
+	$session->setId(1);
+	$session->setUserId($userId);
+	$session->setIpAddress(123);
+	$session->setUserAgent('');
+	$session->setSecondsCreated(time());
+	$session->setSecondsLastUsed(time());
+	$session->setDomain('');
+	$session->setSessionData('');
+	SessionManager::getManager()->userSession = $session;
 
 
-		// eval(\Psy\sh());
-		import('lib.pkp.classes.security.authorization.UserRolesRequiredPolicy');
-		$handler->addPolicy(new UserRolesRequiredPolicy($request));
+	// eval(\Psy\sh());
+	import('lib.pkp.classes.security.authorization.UserRolesRequiredPolicy');
+	$handler->addPolicy(new UserRolesRequiredPolicy($request));
 
-		// Set up the API handler
-		$router->setHandler($handler);
-		$request->setRouter($router);
+	// Set up the API handler
+	$router->setHandler($handler);
+	$request->setRouter($router);
 
-		// Fake the request object
-		$method = 'GET';
-		$uri = \Slim\Http\Uri::createFromString('/publicknowledge/api/v1/contexts');
-		$handler->getApp()->add(function($request, $response, $next) use ($method, $uri) {
+	// Fake the request object
+	$method = 'GET';
+	$uri = \Slim\Http\Uri::createFromString('/publicknowledge/api/v1/contexts');
+	$handler->getApp()->add(function($request, $response, $next) use ($method, $uri) {
 
-			$request = $request->withMethod($method);
-			$request = $request->withUri($uri);
-			// if there were to be a POST body
-			// $request = $request->write(json_encode(['key' => 'val']));
-			// $request->getBody()->rewind();
+	    $request = $request->withMethod($method);
+	    $request = $request->withUri($uri);
+	    // if there were to be a POST body
+	    // $request = $request->write(json_encode(['key' => 'val']));
+	    // $request->getBody()->rewind();
 
-			return $next($request, $response);
-		});
+	    return $next($request, $response);
+	});
 
-		// Run the route
-		$handler->getApp()->run();
+	// Run the route
+	$handler->getApp()->run();
 
-		// import('lib.pkp.api.v1.submissions.PKPSubmissionHandler');
-		// $test = new PKPSubmissionHandler();
-		// eval(\Psy\sh());
-		echo "You specified the command $this->command\n";
-	}
+	// import('lib.pkp.api.v1.submissions.PKPSubmissionHandler');
+	// $test = new PKPSubmissionHandler();
+	// eval(\Psy\sh());
+	echo "You specified the command $this->command\n";
+    }
+
 }
 
 $tool = new CLITool(isset($argv) ? $argv : array());
